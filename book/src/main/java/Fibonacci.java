c3a96039ef28c8e070432a131ed02c13b2ff6fca
Danylo <beldan132@gmail.com>
2024-12-24 23:03:39 +0200

    Improvement in Fibonacci.java task

--- book/src/main/java/Fibonacci.java
+++ book/src/main/java/Fibonacci.java
@@ -1,13 +1,11 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(getFibonacci(3));
+        System.out.println(getFibonacci(5));
     }
     public static String getFibonacci(int num){
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(1);
+        List<Integer> numbers = new ArrayList<>(List.of(1,1));
         for (int i = 2; i < num; i++){
             numbers.add(i, numbers.get(i-1) + numbers.get(i-2));
         }
